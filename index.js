8dccddcb05e8d913c3c5a4a78785f4b162e17d52
Serpel <serpel.js@gmail.com>
2018-10-24 03:40:34 -0600

    default port ngix

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ var app = express();
 app.use(cors())
    .use(cookieParser());
 
-var server = app.listen(process.env.PORT || 8888, function(){
+var server = app.listen(process.env.PORT || 8081, function(){
 
     if (result.error) {
         throw result.error

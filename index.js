1ead2ab9958acc9b972d5e0baf0f67c190fa75f0
Serpel <serpel.js@gmail.com>
2018-10-24 08:37:51 -0600

    updated env file

--- index.js
+++ index.js
@@ -137,7 +137,7 @@ app.get('/callback', function(req, res) {
           });
 
           // we can also pass the token to the browser to make requests from there
-          res.redirect(process.env.REDIRECT_URI+'/playlist?'+ querystring.stringify({
+          res.redirect(process.env.FRONTEND_URI+'/playlist?'+ querystring.stringify({
             access_token: access_token,
             refresh_token: refresh_token
           }));
